6e4c945508f98837aa7635e271c9907de813b35d
hee000 <79568286+hee000@users.noreply.github.com>
2022-06-23 17:25:28 +0900

    Initial Commit

---
+++ Formkdiary/FormkdiaryApp.swift
@@ -0,0 +1,20 @@
+//
+//  FormkdiaryApp.swift
+//  Formkdiary
+//
+//  Created by cch on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FormkdiaryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
